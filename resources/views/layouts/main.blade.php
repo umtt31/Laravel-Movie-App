002341200c8e0d4e1ac61d689b3770c038e8e3c6
umtt31 <umutcan.adiguzel@outlook.com>
2024-04-28 17:11:32 +0300

    initial commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movie App</title>
+    <link rel="stylesheet" href="../../css/app.css">
+</head>
+<body>
+    <nav>Nav goes here</nav>
+    @yield('content')
+</body>
+</html>
